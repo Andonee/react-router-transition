17247c809a3bff4b053ba3be0d7326aa781ae26d
mpoprawa <mateusz.poprawa@gmail.com>
2021-08-30 20:54:47 +0200

    create basic app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg'
 import './App.css'
+import { Route, NavLink, Switch } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<div className='nav'>
+				<NavLink exact to='/' activeClassName='active'>
+					Home
+				</NavLink>
+				<NavLink to='/about' activeClassName='active'>
+					About
+				</NavLink>
+			</div>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route path='/about' component={About} />
+			</Switch>
+		</div>
+	)
 }
 
 export default App
